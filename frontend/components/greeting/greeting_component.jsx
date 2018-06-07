b5125c70c456de81972479acd3643708084e4af2
Valerie Sui <valerie.k.sui@gmail.com>
2018-06-06 21:01:01 -0700

    Finish Nav Bar

--- frontend/components/greeting/greeting_component.jsx
+++ frontend/components/greeting/greeting_component.jsx
@@ -4,19 +4,18 @@ import { Link, Redirect } from 'react-router-dom';
 const Greeting = ({ currentUser, logoutUser }) => {
     // debugger
     const sessionLinks = () => (
-        <div className='navBar'>
-            <nav className="login-signup">
+        <div className='navBar-big'>
+            <div className='navBar-small'>
                 <div className = 'navBar-logo'></div>
                 <div className = 'navBar-buttons'>
                     <div className='nav-button'>
-                        <Link to="/login">Login</Link>
+                        <Link className='nav-button-link' to="/login">Login</Link>
                     </div>
-                    &nbsp;or&nbsp;
                     <div className='nav-button'>
-                        <Link to="/signup">Sign up!</Link>
+                        <Link className='nav-button-link' to="/signup">Sign up</Link>
                     </div>
                 </div>
-            </nav>
+            </div>
         </div>
     );
     
@@ -27,10 +26,17 @@ const Greeting = ({ currentUser, logoutUser }) => {
 
     const personalGreeting = () => (
         <hgroup className="header-group">
-            <h2 className="header-name">Hi, {`${currentUser.first_name} ${currentUser.last_name}`}!</h2>
             <div className='navBar'>
-                <div className='navBar-logo'></div>
-                <button className="header-logout-button" onClick={(e) => handleClick(e)}>Log Out</button>
+                <div className='navBar-small'>
+                    <div className='navBar-logo'></div>
+                    <button className="nav-button" onClick={(e) => handleClick(e)}>Log Out</button>
+                </div>
+            </div>
+            <div className='header-login'>
+                <div className = 'user-picture'>
+                    <img src="https://media.artoyz.net/shop/24449-large_default/14-wooly-stache-labbit.jpg"/>
+                </div>
+                <h2 className="header-name">Hi, {`${currentUser.first_name} ${currentUser.last_name}`}!</h2>
             </div>
         </hgroup>
     );
